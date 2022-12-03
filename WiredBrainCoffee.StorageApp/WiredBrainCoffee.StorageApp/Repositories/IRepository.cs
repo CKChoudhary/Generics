4b1f3c46d4199918d0941eaa62b7a8323c34651b
Chanchal Choudhary <Chanchal.Choudhary@fcsamerica.com>
2022-12-03 15:28:39 -0600

    covariance

--- WiredBrainCoffee.StorageApp/WiredBrainCoffee.StorageApp/Repositories/IRepository.cs
+++ WiredBrainCoffee.StorageApp/WiredBrainCoffee.StorageApp/Repositories/IRepository.cs
@@ -4,10 +4,15 @@ using WiredBrainCoffee.StorageApp.Entities;
 
 namespace WiredBrainCoffee.StorageApp.Repositories
 {
-    public interface IRepository<T> where T :IEntity
+
+    public interface IReadRepository<out T>
     {
-        IEnumerable<T>  GetAll();
+        IEnumerable<T> GetAll();
         T GetById(int id);
+    }
+
+    public interface IRepository<T>: IReadRepository<T> where T :IEntity
+    {
         void Add(T item);
         void Save();
         void Remove(T item);
