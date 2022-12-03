ce11424b9482674e690c82fe3e94c0c198a21dcd
Chanchal Choudhary <Chanchal.Choudhary@fcsamerica.com>
2022-12-02 21:32:40 -0600

    first stack implementation

--- WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/SimpleStack.cs
+++ WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -1,14 +1,18 @@
-﻿namespace WiredBrainCoffee.StackApp
+﻿
+
+namespace WiredBrainCoffee.StackApp
 {
     public class SimpleStack
     {
-        public SimpleStack()
-        {
-        }
+        private readonly double[] _items;
+        private int _currentIndex = -1;
 
-        public void Push(double item)
-        {
-            throw new System.NotImplementedException();
-        }
+        public SimpleStack() => _items = new double[10];
+
+        public int Count => _currentIndex + 1;
+        public double Pop() => _items[_currentIndex--];
+       
+
+        public void Push(double item) => _items[++_currentIndex] = item;
     }
 }
