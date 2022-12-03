81a4ac708be872d826d75dc0e43d845a9cdee8b7
Chanchal Choudhary <Chanchal.Choudhary@fcsamerica.com>
2022-12-03 07:26:42 -0600

    initial repo

---
+++ WiredBrainCoffee.StorageApp/WiredBrainCoffee.StorageApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+using WiredBrainCoffee.StorageApp.Entities;
+using WiredBrainCoffee.StorageApp.Repositories;
+
+namespace WiredBrainCoffee.StorageApp
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            var employeeRepository = new EmployeeRepository();
+            employeeRepository.Add(new Employee(){FirstName = "test"});
+            employeeRepository.Save();
+            Console.ReadLine();
+        }
+    }
+}
