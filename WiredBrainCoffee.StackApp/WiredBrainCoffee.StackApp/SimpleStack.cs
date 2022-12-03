7d2b51621d3fd33fcafe46ad80a6e4b018c98090
Chanchal Choudhary <Chanchal.Choudhary@fcsamerica.com>
2022-12-02 21:10:01 -0600

    initial checkin

---
+++ WiredBrainCoffee.StackApp/WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -0,0 +1,14 @@
+﻿namespace WiredBrainCoffee.StackApp
+{
+    public class SimpleStack
+    {
+        public SimpleStack()
+        {
+        }
+
+        public void Push(double item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
